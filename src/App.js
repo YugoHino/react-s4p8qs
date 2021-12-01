30ae930368cb7e32ead8ef16b8441b17c02e6c8a
YugoHino <henom06@gmail.com>
2021-12-01 14:34:26 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,15 +3,254 @@ import './style.css';
 import * as event_utils from './js/event_utils';
 
 export default function App() {
+  // let events = {};
   return (
-    <div>
+    <div id="div1">
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
 (() => {
-  let events = {};
+  // let events = {};
+  const events = {
+    record: {
+      record_no: { value: 1 },
+      teian_busho: { value: '1.企画部企画グル' },
+      FY: { value: '21' },
+      record_no: { value: 11 },
+      anken_name: { value: 'test_anken' },
+      anken_biko: {
+        value:
+          '備考ですあああああああああああああああああああああああああああああああああああああああああ',
+      },
+      hiyo_total: { value: 1470000 },
+      it_kaihatsu_ichiji: { value: 10000 },
+      it_kaihatsu_uneihi: { value: 20000 },
+      it_uneihi_ty: { value: 20000 },
+      it_uneihi_ny: { value: 20000 },
+      it_uneihi_n2y: { value: 30000 },
+      it_uneihi_n3y: { value: 40000 },
+      kessai_summary: { value: '決裁サマリてすとの文章です' },
+      project_no: { value: 'pjnoxxx/xxx' },
+      project_name: { value: '統合データ分析基盤' },
+      system_no: { value: 'sysno/xxx' },
+      haikei: { value: 'このプロジェクトの背景です' },
+      jisshi_naiyo: {
+        value:
+          'GitHub Enterpriseライセンスの購入\n・9月 15ライセンス（Enterprise/Organization管理者分）\n・10月 50ライセンス（研究開発本部、製薬技術本部分）',
+      },
+      file_mitsumori: {
+        value: [
+          {
+            name: 'test.docx',
+          },
+          {
+            name: 'test1.docx',
+          },
+        ],
+      },
+      file_torihikisaki_sentei: {
+        value: [
+          {
+            name: 'torihiki.docx',
+          },
+        ],
+      },
+      file_sonota: {
+        value: [],
+      },
+      yosan_sochi: {
+        value: [
+          {
+            value: {
+              yosan_kamoku: {
+                value: 'IT開発費',
+              },
+              yosan_kingaku: {
+                value: '80',
+              },
+              yosan_kubun: {
+                value: 'その他経費',
+              },
+            },
+          },
+          {
+            value: {
+              yosan_kamoku: {
+                value: 'IT開発費2',
+              },
+              yosan_kingaku: {
+                value: '20',
+              },
+              yosan_kubun: {
+                value: 'その他経費2',
+              },
+            },
+          },
+        ],
+      },
+      yosan_keijo: {
+        value: '3.予算未計上',
+      },
+      yosan_riyu: {
+        value: '',
+      },
+      hiyo_table: {
+        value: [
+          {
+            value: {
+              hiyo01: {
+                value: '100',
+              },
+              hiyo02: {
+                value: '200',
+              },
+              hiyo03_0: {
+                value: '310',
+              },
+              hiyo03: {
+                value: '300',
+              },
+              hiyo04: {
+                value: '400',
+              },
+              hiyo05: {
+                value: '500',
+              },
+              hiyo_biko: {
+                value: 'テスト備考',
+              },
+              hiyo_nounyuyotei: {
+                value: '2021-01-01',
+              },
+              hiyo_kei: {
+                value: '1000',
+              },
+              hiyo_naiyo: {
+                value: 'ライセンス2',
+              },
+              hiyo_torihikisaki: {
+                value: '企画株式会社A',
+              },
+            },
+          },
+          {
+            value: {
+              hiyo01: {
+                value: '100',
+              },
+              hiyo02: {
+                value: '200',
+              },
+              hiyo03_0: {
+                value: '310',
+              },
+              hiyo03: {
+                value: '300',
+              },
+              hiyo04: {
+                value: '400',
+              },
+              hiyo05: {
+                value: '500',
+              },
+              hiyo_biko: {
+                value: 'テスト備考',
+              },
+              hiyo_nounyuyotei: {
+                value: '2021-08-01',
+              },
+              hiyo_kei: {
+                value: '1000',
+              },
+              hiyo_naiyo: {
+                value: 'ライセンス2',
+              },
+              hiyo_torihikisaki: {
+                value: '企画株式会社A',
+              },
+            },
+          },
+          {
+            value: {
+              hiyo01: {
+                value: '100',
+              },
+              hiyo02: {
+                value: '200',
+              },
+              hiyo03_0: {
+                value: '310',
+              },
+              hiyo03: {
+                value: '300',
+              },
+              hiyo04: {
+                value: '400',
+              },
+              hiyo05: {
+                value: '500',
+              },
+              hiyo_biko: {
+                value: 'テスト備考',
+              },
+              hiyo_nounyuyotei: {
+                value: '2021-07-01',
+              },
+              hiyo_kei: {
+                value: '1000',
+              },
+              hiyo_naiyo: {
+                value: 'ライセンス１',
+              },
+              hiyo_torihikisaki: {
+                value: '企画株式会社A',
+              },
+            },
+          },
+          {
+            value: {
+              hiyo01: {
+                value: '100',
+              },
+              hiyo02: {
+                value: '200',
+              },
+              hiyo03_0: {
+                value: '310',
+              },
+              hiyo03: {
+                value: '300',
+              },
+              hiyo04: {
+                value: '400',
+              },
+              hiyo05: {
+                value: '500',
+              },
+              hiyo_biko: {
+                value: 'テスト備考',
+              },
+              hiyo_nounyuyotei: {
+                value: '2021-07-01',
+              },
+              hiyo_kei: {
+                value: '1000',
+              },
+              hiyo_naiyo: {
+                value: 'ライセンス3',
+              },
+              hiyo_torihikisaki: {
+                value: '企画株式会社b',
+              },
+            },
+          },
+        ],
+      },
+    },
+  };
+
   console.log('ok');
   event_utils.event_kessaitype(events);
 })();
--- src/js/event_utils.js
+++ src/js/event_utils.js
@@ -5,16 +5,26 @@ import * as FileSaver from 'file-saver';
 export function event_kessaitype(event) {
   let record = event.record;
   console.log('レコード情報', event);
+  const kessaiSha = 'gcho';
   // let return_setFieldShown = setFieldShown(event);
   // let kessaiSha = return_setFieldShown[0];
   // let hidden_fields = return_setFieldShown[1];
 
   const docxDLButtonId = 'kessai_download';
+  // const div1 = document.getElementById('div1');
+  // let form = document.createElement('form');
+  // form.id = 'Form';
   var docxButton = document.createElement('button');
+  // let docxButton = document.getElementById('button1');
   // let docxButton = new kintoneUIComponent.Button({ text: '決裁' });
   docxButton.id = 'docx_button';
   docxButton.innerText = '決裁書Wordダウンロード';
-  // kintone.app.record.getSpaceElement(docxDLButtonId).appendChild(docxButton);
+  console.log(docxButton);
+  // form.appendChild(docxButton);
+  // let element = document.getElementsByTagName('p');
+  // element.appendChild(docxButton);
+  document.body.appendChild(docxButton);
+
   // kintone.app.record.getHeaderMenuSpaceElement().appendChild(docxButton);
   docxButton.addEventListener('click', function () {
     docx.Packer.toBlob(word_format.createDoc(record, kessaiSha)).then(
@@ -24,5 +34,6 @@ export function event_kessaitype(event) {
       }
     );
   });
+
   return event;
 }
